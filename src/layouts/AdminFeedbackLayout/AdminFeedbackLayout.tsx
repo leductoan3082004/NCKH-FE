05d20690b7328baab3ad9d3652f12dcae16ede68
teddylethal <teddy.lethal@gmail.com>
2024-02-13 11:54:13 +0700

    Feedback Item

--- src/layouts/AdminFeedbackLayout/AdminFeedbackLayout.tsx
+++ src/layouts/AdminFeedbackLayout/AdminFeedbackLayout.tsx
@@ -1,37 +1,111 @@
 import classNames from 'classnames'
-import React, { useState } from 'react'
+import React, { Fragment, createContext, useState } from 'react'
+import { Feedback } from 'src/types/feedback.type'
 
 interface Props {
   children?: React.ReactNode
 }
 
+export interface ExtendedFeedback extends Feedback {
+  disabled: boolean
+  checked: boolean
+}
+
+interface FeedbackContextInterface {
+  deletingMode: boolean
+  setDeletingMode: React.Dispatch<React.SetStateAction<boolean>>
+  extendedFeedbacks: ExtendedFeedback[]
+  setExtendedFeedbacks: React.Dispatch<React.SetStateAction<ExtendedFeedback[]>>
+}
+
+const initialFeedbackContext: FeedbackContextInterface = {
+  deletingMode: false,
+  setDeletingMode: () => null,
+  extendedFeedbacks: [],
+  setExtendedFeedbacks: () => null
+}
+
+export const FeedbackContext = createContext<FeedbackContextInterface>(initialFeedbackContext)
+
 export default function AdminFeedbackLayout({ children }: Props) {
-  const [deletingMode, setDeletingMode] = useState<boolean>(false)
+  const [deletingMode, setDeletingMode] = useState<boolean>(initialFeedbackContext.deletingMode)
+  const [extendedFeedbacks, setExtendedFeedbacks] = useState<ExtendedFeedback[]>(
+    initialFeedbackContext.extendedFeedbacks
+  )
+
+  //! SELECT FEEDBACK
+  const isAllChecked = extendedFeedbacks.every((feedback) => feedback.checked)
+  const checkedFeedbacksCount = extendedFeedbacks.length
+
+  const cancelDelete = () => {
+    setExtendedFeedbacks((prev) =>
+      prev.map((feedback) => ({
+        ...feedback,
+        checked: false
+      }))
+    )
+    setDeletingMode(false)
+  }
+
+  const handleSelectAll = () => {
+    setExtendedFeedbacks((prev) =>
+      prev.map((purchase) => ({
+        ...purchase,
+        checked: !isAllChecked
+      }))
+    )
+  }
 
   return (
-    <div className=''>
-      <div className='flex justify-end space-x-4'>
-        <button
-          onClick={() => setDeletingMode(false)}
-          className={classNames(
-            'py-1 visible px-2 text-xs tablet:text-sm tablet:px-4 bg-primaryBackground/80 hover:bg-primaryBackground rounded-lg',
-            {
+    <FeedbackContext.Provider value={{ deletingMode, setDeletingMode, extendedFeedbacks, setExtendedFeedbacks }}>
+      <Fragment>
+        <div className='w-full justify-end space-y-4'>
+          <div className='flex justify-end space-x-10 tablet:space-x-20'>
+            <button
+              onClick={cancelDelete}
+              className={classNames(
+                'py-1 px-2 text-xs tablet:text-sm tablet:px-4 bg-primaryBackground/80 hover:bg-primaryBackground rounded-lg',
+                {
+                  invisible: !deletingMode
+                }
+              )}
+            >
+              Hủy
+            </button>
+
+            <button
+              onClick={() => setDeletingMode(true)}
+              className='py-1  px-2 text-xs tablet:text-sm tablet:px-4 bg-alertRed/80 hover:bg-alertRed rounded-lg'
+            >
+              {deletingMode ? 'Xóa' : 'Xóa feedback'}
+            </button>
+          </div>
+          <div
+            className={classNames('flex text-xs  tablettext-sm w-full space-x-4 justify-end', {
               invisible: !deletingMode
-            }
-          )}
-        >
-          Hủy
-        </button>
-        <button
-          onClick={() => setDeletingMode(true)}
-          className='py-1  px-2 text-xs tablet:text-sm tablet:px-4 bg-alertRed/80 hover:bg-alertRed rounded-lg'
-        >
-          {deletingMode ? 'Xóa' : 'Xóa feedback'}
-        </button>
-      </div>
-      <div className='mt-4 tablet:mt-6 desktop:mt-8 border border-black/40 rounded-lg py-2 tablet:py-4 desktop:py-6 px-2 tablet:px-4'>
-        {children}
-      </div>
-    </div>
+            })}
+          >
+            <div className='items-center flex space-x-1'>
+              <input
+                name='is_selected'
+                type='checkbox'
+                className='h-5 w-5 accent-primaryBackground'
+                checked={isAllChecked}
+                onChange={handleSelectAll}
+              />
+              <span className='italic font-bold'>Chọn tất cả</span>
+            </div>
+            <div className='flex items-center space-x-1 '>
+              <span className='italic'>Đã chọn:</span>
+              <span className='font-bold'>{checkedFeedbacksCount}</span>
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-4 tablet:mt-6 desktop:mt-8 border border-black/40 rounded-lg py-2 tablet:py-4 desktop:py-6 px-2 tablet:px-4'>
+          {children}
+        </div>
+      </Fragment>
+    </FeedbackContext.Provider>
   )
 }
--- src/pages/AdminPage/components/AdminFeedbackManagement/AdminFeedbackManagement.tsx
+++ src/pages/AdminPage/components/AdminFeedbackManagement/AdminFeedbackManagement.tsx
@@ -1,52 +1,55 @@
 import { useQuery } from '@tanstack/react-query'
-import { Fragment, useState } from 'react'
+import { produce } from 'immer'
+import { keyBy } from 'lodash'
+import { Fragment, useContext, useEffect } from 'react'
 import { ColorRing } from 'react-loader-spinner'
 import { feedbackApi } from 'src/apis/feedback.api'
 import useFeedbackListQueryConfig from 'src/hooks/useFeedbackListQueryConfig'
 import { useViewport } from 'src/hooks/useViewport'
-import { Feedback, FeedbackListConfig } from 'src/types/feedback.type'
+import { ExtendedFeedback, FeedbackContext } from 'src/layouts/AdminFeedbackLayout/AdminFeedbackLayout'
+import { FeedbackListConfig } from 'src/types/feedback.type'
 
-const LargeFeedbackItem = ({ feedback }: { feedback: Feedback }) => {
-  const [hovering, setHovering] = useState<boolean>(false)
+interface ItemProps {
+  feedback: ExtendedFeedback
+  index: number
+  handleChecking: (feedbackIndex: number) => (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const LargeFeedbackItem = ({ feedback, handleChecking, index }: ItemProps) => {
+  const { deletingMode } = useContext(FeedbackContext)
 
   return (
-    <div
-      className='w-full relative border border-black/40 rounded-md overflow-hidden'
-      onMouseEnter={() => setHovering(true)}
-      onMouseLeave={() => setHovering(false)}
-    >
+    <div className='w-full relative border border-black/40 rounded-md overflow-hidden'>
       <div className='grid grid-cols-12 gap-2 desktopLarge:gap-4 py-4 px-4'>
         <div className='col-span-8 space-y-4'>
           <p className='font-bold text-lg desktopLarge:text-xl'>{feedback.topic}</p>
           <div className='w-full h-20 desktopLarge:h-40 overflow-hidden'>{feedback.content}</div>
         </div>
-        <div className='col-span-4 space-y-4 border-l border-black/20 pl-4'>
-          <div className='grid grid-cols-4'>
-            <div className='col-span-1'>Người đăng</div>
-            <div className='col-span-3'>{feedback.name}</div>
-          </div>
-          <div className='grid grid-cols-4'>
-            <div className='col-span-1'>Email</div>
-            <div className='col-span-3'>{feedback.email}</div>
-          </div>
+        <div className='col-span-4 space-y-4 border-l text-sm md border-black/20 pl-4'>
+          <div className='italic'>{feedback.name}</div>
+          <div className='italic'>{feedback.email}</div>
         </div>
       </div>
-      {hovering && (
-        <div className='absolute w-10/12 bg-black/80 bottom-1 rounded-md px-2 py-1 left-1/2 -translate-x-1/2 flex items-center space-x-1 text-white text-xs'></div>
+      {deletingMode && (
+        <div className='absolute top-2 right-2'>
+          <input
+            name='is_selected'
+            type='checkbox'
+            className='h-5 w-5 accent-primaryBackground'
+            checked={feedback.checked}
+            onChange={handleChecking(index)}
+          />
+        </div>
       )}
     </div>
   )
 }
 
-const SmallFeedbackItem = ({ feedback }: { feedback: Feedback }) => {
-  const [hovering, setHovering] = useState<boolean>(false)
+const SmallFeedbackItem = ({ feedback, index, handleChecking }: ItemProps) => {
+  const { deletingMode } = useContext(FeedbackContext)
 
   return (
-    <div
-      className='w-full relative border border-black/40 rounded-md overflow-hidden'
-      onMouseEnter={() => setHovering(true)}
-      onMouseLeave={() => setHovering(false)}
-    >
+    <div className='w-full relative border border-black/40 rounded-md overflow-hidden'>
       <div className='p-2'>
         <div className='space-y-2'>
           <p className='font-bold text-base tabletSmall:text-lg'>{feedback.topic}</p>
@@ -55,14 +58,24 @@ const SmallFeedbackItem = ({ feedback }: { feedback: Feedback }) => {
           <p className='font-thin text-sm italic'>{feedback.email}</p>
         </div>
       </div>
-      {hovering && (
-        <div className='absolute w-10/12 bg-black/80 bottom-1 rounded-md px-2 py-1 left-1/2 -translate-x-1/2 flex items-center space-x-1 text-white text-xs'></div>
+      {deletingMode && (
+        <div className='absolute top-2 right-2'>
+          <input
+            name='is_selected'
+            type='checkbox'
+            className='h-5 w-5 accent-primaryBackground'
+            checked={feedback.checked}
+            onChange={handleChecking(index)}
+          />
+        </div>
       )}
     </div>
   )
 }
 
 export default function AdminFeedbackManagement() {
+  const { extendedFeedbacks, setExtendedFeedbacks } = useContext(FeedbackContext)
+
   const isMobile = useViewport().width < 768
 
   //! GET FEEDBACK LIST
@@ -75,8 +88,32 @@ export default function AdminFeedbackManagement() {
     },
     staleTime: 3 * 60 * 1000
   })
+  const feedbackList = feedbackData?.data.data
 
-  //! DELETE FEEDBACK
+  useEffect(() => {
+    setExtendedFeedbacks((prev) => {
+      const extendedPurchasesObject = keyBy(prev, '_id')
+      return (
+        feedbackList?.map((feedback) => ({
+          ...feedback,
+          disabled: false,
+          checked: Boolean(extendedPurchasesObject[feedback._id]?.checked)
+        })) || []
+      )
+    })
+  }, [feedbackList, setExtendedFeedbacks])
+
+  //! HANDLE SELECT FEEDBACK
+
+  const handleChecking = (feedbackIndex: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    setExtendedFeedbacks(
+      produce((draft) => {
+        draft[feedbackIndex].checked = event.target.checked
+      })
+    )
+  }
+
+  //! HANDLE DELETE FEEDBACK
 
   return (
     <div className=''>
@@ -93,22 +130,22 @@ export default function AdminFeedbackManagement() {
           />
         </div>
       )}
-      {feedbackData && (
+      {extendedFeedbacks.length > 0 && (
         <Fragment>
           {!isMobile && (
             <div className='flex flex-col space-y-4'>
-              {feedbackData?.data.data.map((feedback) => (
+              {extendedFeedbacks.map((feedback, index) => (
                 <div className='' key={feedback._id}>
-                  <LargeFeedbackItem feedback={feedback} />
+                  <LargeFeedbackItem feedback={feedback} index={index} handleChecking={handleChecking} />
                 </div>
               ))}
             </div>
           )}
           {isMobile && (
             <div className='flex flex-col space-y-2'>
-              {feedbackData?.data.data.map((feedback) => (
+              {extendedFeedbacks.map((feedback, index) => (
                 <div className='' key={feedback._id}>
-                  <SmallFeedbackItem feedback={feedback} />
+                  <SmallFeedbackItem feedback={feedback} index={index} handleChecking={handleChecking} />
                 </div>
               ))}
             </div>
